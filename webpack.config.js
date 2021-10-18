341184be5dfd9e5915aa691e5e76086f378ed3ae
Duncan Perkins <duncan.t.perkins@gmail.com>
2021-10-17 22:33:21 -0400

    'sdf'

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,24 @@
-const path = require( 'path' );
+const path = require('path');
 
 module.exports = {
-    // entry files
-    entry: './main.ts',
-   
-
-    // bundling mode
-    mode: 'production',
-    target: "node",
-    node: {
-        __dirname: false
-    },
-
-    // output bundles (location)
-    output: {
-        filename: '[name].[hash].bundle.js',
-        path: path.resolve( __dirname, 'build' ),
-        chunkFilename: '[name].[hash].bundle.js'
-    },
-    optimization: {
-        runtimeChunk: "single",
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    chunks: 'initial',
-                    name: 'vendors',
-                    test: /[\\/]node_modules[\\/]/,
-                    enforce: true
-                }
-            }
-        }
-    },
-
-    // file resolutions
-    resolve: {
-        modules : [
-            'node_modules'
-        ],
-        extensions: [ '.ts', '.js', '.json'],
-    },
-
-    // loaders
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-            },
-            {
-                test: /\.node$/,
-                use: 'node-loader'
-            }
-        ]
-    }
+  entry: './main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      },
+      {
+          test: /\node&/,
+          use: 'node-loader'
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build'),
+  },
 };
