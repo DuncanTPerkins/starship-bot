b7324ed91e396513eba2fcf17870d9cd91ff0c29
Duncan Perkins <duncan.t.perkins@gmail.com>
2021-10-17 20:34:22 -0400

    sdfsdf

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Routes } from 'discord-api-types/v9.js';
+const Routes = require('discord-api-types/v9');
 import { REST } from '@discordjs/rest';
 import { Secrets } from './src/secrets';
 import { Client, Intents } from 'discord.js';
