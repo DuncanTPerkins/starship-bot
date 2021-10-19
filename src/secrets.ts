ea48f87207d97c4407af9769a10a16b23be1e434
Duncan Perkins <duncan.t.perkins@gmail.com>
2021-10-18 21:24:48 -0400

    .gitignore fix

--- src/secrets.ts
+++
@@ -1,6 +0,0 @@
-export const env = {
-    token: 'ODk5MzIwNTQ3ODc4NTM1MTY4.YWxDhw.TJ_IQS9P5Kzibb8dniCPr7iahRA',
-    clientId: '899320547878535168',
-    clientSecret: 'WLR-Najn_uM-_2Ng21Qnc64Jml9bbCQY',
-    starshipId: '721238519841816598'
-};
