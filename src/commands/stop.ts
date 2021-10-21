77414e20c9a89b300c90729cce04882ccdbf5fea
Duncan Perkins <duncan.t.perkins@gmail.com>
2021-10-21 17:46:01 -0400

    fix stop

--- src/commands/stop.ts
+++ src/commands/stop.ts
@@ -1,11 +1,13 @@
 import { CommandInteraction } from "discord.js/typings/index.js";
 import { checkMC, getWrongMcResponse } from "./mc";
-import { stopAudioPlayer } from "./player";
 import { AudioStreamer } from "../audio-streamer/audio-streamer";
+import { CommonEmbeds } from "../common/common-embeds";
 
 export async function stop(interaction: CommandInteraction) {
     if (await checkMC(interaction.channelId) === false) {
         await getWrongMcResponse(interaction);
         return;
     }
+    AudioStreamer.get().stop();
+    await interaction.reply('\`🤧 Music stopped\`');
 };
