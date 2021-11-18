0c6ee49abbfc4187b48188e1eb6d8728c0591e50
Duncan Perkins <duncan.t.perkins@gmail.com>
2021-11-17 21:46:06 -0500

    new imports

--- main.ts
+++ main.ts
@@ -5,13 +5,23 @@ import { Client } from 'discord.js';
 import { commands, execCommand } from './src/commands/commands';
 import { initDb } from './src/db/db';
 import { Secrets } from './src/secrets';
+import { SpotifyWebApi } from 'spotify-web-api-ts';
 
 const rest = new REST({ version: '9' }).setToken(Secrets.env.token || '');
 const client = new Client({ intents: ['GUILDS', 'GUILD_VOICE_STATES', 'GUILD_MESSAGES',] });
-
+const spotify = new SpotifyWebApi({
+  clientId: Secrets.env.spotifyClientId,
+  clientSecret: Secrets.env.spotifySecret
+})
 client.on('ready', () => {
   console.log(`Logged in as ${client.user?.tag}!`);
   initDb();
+  try {
+    spotify.playlists.getPlaylistItems('37i9dQZF1E3aiWKbPI1NqK')
+    .then(x => console.log(x));
+  } catch(e) {
+    console.log(e);
+  }
 });
 
 client.on('interactionCreate', async interaction => {
